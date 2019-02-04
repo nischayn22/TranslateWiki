7ec348483144d7e07b5307f7437843076c8bbfcd
Nischayn22 <nischayn22@gmail.com>
2019-02-04 12:47:43 +0530

    Added Working LinkTranslations

--- SpecialLinkTranslations.php
+++ SpecialLinkTranslations.php
@@ -34,18 +34,6 @@ class SpecialLinkTranslations extends QueryPage {
 			return;
 		}
 
-		$linkDefs = [
-			'Add Link Translations' => 'Special:LinkTranslations',
-			'Edit Link Translations' => 'Special:LinkTranslations/edit'
-		];
-
-		$links = [];
-		foreach ( $linkDefs as $name => $page ) {
-			$title = Title::newFromText( $page );
-			$links[] = $this->getLinkRenderer()->makeLink( $title, new HtmlArmor( $name ) );
-		}
-		$linkStr = $this->getContext()->getLanguage()->pipeList( $links );
-		$out->setSubtitle( $linkStr );
 
 		$this->lang = $request->getVal( 'lang' );
 
@@ -70,10 +58,8 @@ class SpecialLinkTranslations extends QueryPage {
 					)
 				);
 			}
-			$start_msg = "Start Adding";
-			if ( $subpage == "edit" ) {
-				$start_msg = "Start Editing";
-			}
+			$start_msg = "Start";
+
 			$out->addHTML( Html::closeElement( 'select' ) . "<br>" );
 			$out->addHTML(
 				"<br>" .
@@ -84,225 +70,96 @@ class SpecialLinkTranslations extends QueryPage {
 		}
 
 		$page_action = $request->getVal( 'page_action' );
-		if ( $page_action == 'save_correction' ) {
-			$this->saveCorrection();
-			$out->addHTML( '<div style="background-color:#28dc28;color:white;padding:5px;">Correction Saved.</div>' );
-		} else if ( $page_action == 'edit_corrections' ) {
-			$update = $request->getVal( 'update' );
-			if ( $update != '' ) {
-				$update_status = $this->editCorrection();
-				if ( $update_status ) {
-					$out->addHTML( '<div style="background-color:#28dc28;color:white;padding:5px;">Correction updated successfully!</div>' );
-				} else {
-					$out->addHTML( '<div style="background-color:red;color:white;padding:5px;">Update failed!</div>' );
-				}
-			} else {
-				$update_status = $this->deleteCorrection();
-				if ( $update_status ) {
-					$out->addHTML( '<div style="background-color:#28dc28;color:white;padding:5px;">Correction deleted successfully!</div>' );
-				} else {
-					$out->addHTML( '<div style="background-color:red;color:white;padding:5px;">Delete failed!</div>' );
-				}
+		$url = $request->getVal( 'url' );
+		$this->search_pattern = $request->getVal( 'search_pattern' );
+
+		if ( empty( $url ) ) {
+			if ( $page_action == 'save_translation' ) {
+				$this->saveTranslation();
+				$out->addHTML( '<div style="background-color:#28dc28;color:white;padding:5px;">Translation Saved.</div>' );
+			} else if ( $page_action == 'update_translation' ) {
+				$this->updateTranslation();
+				$out->addHTML( '<div style="background-color:#28dc28;color:white;padding:5px;">Translation Updated.</div>' );
 			}
 		}
 
-		if ( $subpage != 'edit' ) {
-			$el_id = $request->getVal( 'el_id' );
-			$this->search_pattern = $request->getVal( 'search_pattern' );
-
-			if ( !empty( $el_id ) ) {
-				$formOpts = [
-					'id' => 'add_translation',
-					'method' => 'post',
-					'action' => $this->getTitle()->getFullUrl()
-				];
-
-				$out->addHTML(
-					Html::openElement( 'form', $formOpts ) . "<br>" .
-					Html::element( 'input', [ 'name' => 'lang', 'value' => $this->lang, 'type' => 'hidden' ] ) .
-					'<h5>'. $this->search_pattern .'</h5>' . 
-					Html::label( "Translated String:","", array( "for" => "translated_str" ) ) . "<br>" .
-					Html::textarea( "translated_str", "" ) . "<br>" .
-					Html::element( 'input', [ 'name' => 'page_action', 'value' => 'save_translation', 'type' => 'hidden' ] ) .
-					Html::submitButton( "Save Translation", array() ) .
-					Html::closeElement( 'form' )
-				);
-			} else {
-				$protocols_list = [];
-				foreach ( $this->getConfig()->get( 'UrlProtocols' ) as $prot ) {
-					if ( $prot !== '//' ) {
-						$protocols_list[] = $prot;
-					}
-				}
-				$out->addWikiMsg(
-					'linksearch-text',
-					'<nowiki>' . $this->getLanguage()->commaList( $protocols_list ) . '</nowiki>',
-					count( $protocols_list )
-				);
-
-				$fields = [
-					'search_pattern' => [
-						'type' => 'text',
-						'name' => 'search_pattern',
-						'id' => 'search_pattern',
-						'size' => 50,
-						'label-message' => 'linksearch-pat',
-						'default' => $this->search_pattern,
-						'dir' => 'ltr',
-					]
-				];
-				$htmlForm = HTMLForm::factory( 'ooui', $fields, $this->getContext() );
-				$htmlForm->addHiddenField( 'lang', $this->lang );
-				$htmlForm->setAction( $this->getTitle()->getFullUrl() . "/" . $subpage );
-				$htmlForm->setMethod( 'get' );
-				$htmlForm->prepareForm()->displayForm( false );
-
-				if ( !empty( $this->search_pattern ) ) {
-					$target2 = $this->search_pattern;
-					// Get protocol, default is http://
-					$protocol = 'http://';
-					$bits = wfParseUrl( $this->search_pattern );
-					if ( isset( $bits['scheme'] ) && isset( $bits['delimiter'] ) ) {
-						$protocol = $bits['scheme'] . $bits['delimiter'];
-						// Make sure wfParseUrl() didn't make some well-intended correction in the
-						// protocol
-						if ( strcasecmp( $protocol, substr( $this->search_pattern, 0, strlen( $protocol ) ) ) === 0 ) {
-							$target2 = substr( $this->search_pattern, strlen( $protocol ) );
-						} else {
-							// If it did, let LinkFilter::makeLikeArray() handle this
-							$protocol = '';
-						}
-					}
-					$this->protocol = $protocol;
-
-					$this->setParams( [
-						'query' => Parser::normalizeLinkUrl( $target2 ),
-						'protocol' => $protocol 
-					] );
-					parent::execute( $subpage );
-					if ( $this->mungedQuery === false ) {
-						$out->addWikiMsg( 'linksearch-error' );
-					}
-				}
-			}
-		} else {
+		if ( !empty( $url ) ) {
+			$urlLink = Linker::makeExternalLink( $url, $url );
 			$formOpts = [
-				'id' => 'select_range',
-				'method' => 'get',
-				'action' => $this->getTitle()->getFullUrl() . "/" . $subpage
+				'id' => 'add_translation',
+				'method' => 'post',
+				'action' => $this->getTitle()->getFullUrl()
 			];
 
 			$out->addHTML(
 				Html::openElement( 'form', $formOpts ) . "<br>" .
 				Html::element( 'input', [ 'name' => 'lang', 'value' => $this->lang, 'type' => 'hidden' ] ) .
-				Html::label( "Select a range:","", array( "for" => "page_offset" ) ) . "<br>" .
-				Html::openElement( 'select', array( "id" => "page_offset", "name" => "page_offset", "style" => "width:100%;" ) )
-			);
-
-			$dbr = wfGetDB( DB_SLAVE );
-			$corrections_count = $dbr->selectField( 
-				LinkTranslations::TABLE,
-				[ 'COUNT(*)' ],
-				[ 'true' ],
-				__METHOD__
-			);
-
-			$limit = 20;
-			if ( $corrections_count < $limit ) {
-				$limit = $corrections_count;
-			}
-			$offsets = range( 0, $corrections_count, $limit );
-			$current_offset = $request->getVal( 'page_offset' );
-			if ( $current_offset == '' ) {
-				$current_offset = 0;
-			}
-
-			foreach( $offsets as $offset ) {
-				$to_offset = min( $offset + $limit, $corrections_count );
-				if ( $offset == $to_offset ) {
-					continue;
-				}
-				$out->addHTML(
-					Html::element(
-						'option', [
-							'selected' => $offset == $current_offset,
-							'value' => $offset,
-						], $offset . ' - ' . $to_offset
-					)
-				);
-			}
-
-			$out->addHTML( Html::closeElement( 'select' ) . "<br>" );
-			$out->addHTML(
-				"<br>" .
-				Html::submitButton( "Get Corrections List", array() ) .
+				Html::element( 'input', [ 'name' => 'original_str', 'value' => $url, 'type' => 'hidden' ] ) .
+				Html::element( 'input', [ 'name' => 'search_pattern', 'value' => $this->search_pattern, 'type' => 'hidden' ] ) .
+				Html::element( 'input', [ 'name' => 'limit', 'value' => $this->limit, 'type' => 'hidden' ] ) .
+				Html::element( 'input', [ 'name' => 'offset', 'value' => $this->offset, 'type' => 'hidden' ] ) .
+				'<span>Url: '. $urlLink .'</span><br>' .
+				Html::label( "Translated String:","", array( "for" => "translated_str" ) ) . "<br>" .
+				Html::textarea( "translated_str", $request->getVal( 'translated_str' ) ) . "<br>" .
+				Html::element( 'input', [ 'name' => 'page_action', 'value' => $page_action, 'type' => 'hidden' ] ) .
+				Html::submitButton( "Save Translation", array() ) .
 				Html::closeElement( 'form' )
 			);
-
-			$dbr = wfGetDB( DB_SLAVE );
-			$res = $dbr->select( 
-				LinkTranslations::TABLE,
-				[ 'id', 'original_str', 'corrected_str' ],
-				[ 'lang' => $this->lang ],
-				__METHOD__,
-				array( 'OFFSET' => $current_offset, 'LIMIT' => $limit )
+		} else {
+			$protocols_list = [];
+			foreach ( $this->getConfig()->get( 'UrlProtocols' ) as $prot ) {
+				if ( $prot !== '//' ) {
+					$protocols_list[] = $prot;
+				}
+			}
+			$out->addWikiMsg(
+				'linksearch-text',
+				'<nowiki>' . $this->getLanguage()->commaList( $protocols_list ) . '</nowiki>',
+				count( $protocols_list )
 			);
 
-			if ( $res->numRows() > 0 ) {
-				$out->addHTML( '
-					<div>
-						<div style="float:left;width:30%;height:40px;">
-							<h4>Original Link</h4>
-						</div>
-						<div style="float:left;margin-left:1%;margin-right:1%;height: 10px;"></div>
-						<div style="float:left;width:30%;height:40px;">
-							<h4>Translated Link</h4>
-						</div>
-						<div style="float:left;margin-left:1%;margin-right:1%;height: 10px;"></div>
-						<div style="float:left;width:30%;height:40px;">
-							<h4>Actions</h4>
-						</div>
-					</div>
-				');
-				foreach ( $res as $row ) {
-					$formOpts = [
-						'id' => 'edit_corrections',
-						'method' => 'post',
-						'action' => $this->getTitle()->getFullUrl() . "/" . $subpage,
-						'style' => 'clear:both;'
-					];
+			$fields = [
+				'search_pattern' => [
+					'type' => 'text',
+					'name' => 'search_pattern',
+					'id' => 'search_pattern',
+					'size' => 50,
+					'label-message' => 'linksearch-pat',
+					'default' => $this->search_pattern,
+					'dir' => 'ltr',
+				]
+			];
+			$htmlForm = HTMLForm::factory( 'ooui', $fields, $this->getContext() );
+			$htmlForm->addHiddenField( 'lang', $this->lang );
+			$htmlForm->setAction( $this->getTitle()->getFullUrl() . "/" . $subpage );
+			$htmlForm->setMethod( 'get' );
+			$htmlForm->prepareForm()->displayForm( false );
 
-					$out->addHTML(
-						"<br>" .
-						Html::openElement( 'form', $formOpts ) .
-						Html::element( 'input', [ 'name' => 'lang', 'value' => $this->lang, 'type' => 'hidden' ] ) .
-						Html::element( 'input', [ 'name' => 'page_offset', 'value' => $current_offset, 'type' => 'hidden' ] ) .
-						Html::element( 'input', [ 'name' => 'page_limit', 'value' => $limit, 'type' => 'hidden' ] ) .
-						Html::element( 'input', [ 'name' => 'update_existing', 'value' => 1, 'type' => 'hidden' ] ) .
-						Html::element( 'input', [ 'name' => 'correction_id', 'value' => $row->id, 'type' => 'hidden' ] ) .
-						Html::element( 'input', [ 'name' => 'page_action', 'value' => 'edit_corrections', 'type' => 'hidden' ] )
-					);
-					$out->addHTML( '
-						<div>
-							<div style="float:left;width:30%;height:100px;">
-								' . Html::textarea( 'original_str', $row->original_str ) . '
-							</div>
-							<div style="float:left;margin-left:1%;margin-right:1%;height: 100px;"></div>
-							<div style="float:left;width:30%;height:100px;">
-								'. Html::textarea( 'corrected_str', $row->corrected_str ) .'
-							</div>
-							<div style="float:left;margin-left:1%;margin-right:1%;height: 100px;"></div>
-							<div style="float:left;width:30%;height:100px;">
-								'. 
-								Html::submitButton( "Update Translation", array( 'name' => 'update' ) ) .
-								'&emsp;' .
-								Html::submitButton( "Delete Translation", array( 'name' => 'delete' ) ) .
-								Html::closeElement( 'form' )
-								.'
-							</div>
-						</div>
-						<br>
-					' );
+			if ( !empty( $this->search_pattern ) ) {
+				$target2 = $this->search_pattern;
+				// Get protocol, default is http://
+				$protocol = 'http://';
+				$bits = wfParseUrl( $this->search_pattern );
+				if ( isset( $bits['scheme'] ) && isset( $bits['delimiter'] ) ) {
+					$protocol = $bits['scheme'] . $bits['delimiter'];
+					// Make sure wfParseUrl() didn't make some well-intended correction in the
+					// protocol
+					if ( strcasecmp( $protocol, substr( $this->search_pattern, 0, strlen( $protocol ) ) ) === 0 ) {
+						$target2 = substr( $this->search_pattern, strlen( $protocol ) );
+					} else {
+						// If it did, let LinkFilter::makeLikeArray() handle this
+						$protocol = '';
+					}
+				}
+				$this->protocol = $protocol;
+
+				$this->setParams( [
+					'query' => Parser::normalizeLinkUrl( $target2 ),
+					'protocol' => $protocol 
+				] );
+				parent::execute( $subpage );
+				if ( $this->mungedQuery === false ) {
+					$out->addWikiMsg( 'linksearch-error' );
 				}
 			}
 		}
@@ -352,18 +209,19 @@ class SpecialLinkTranslations extends QueryPage {
 		$stripped = LinkFilter::keepOneWildcard( $this->mungedQuery );
 		$like = $dbr->buildLike( $stripped );
 		$retval = [
-			'tables' => [ 'page', 'externallinks' ],
+			'tables' => [ 'externallinks', LinkTranslations::TABLE ],
 			'fields' => [
-				'namespace' => 'page_namespace',
-				'title' => 'page_title',
-				'el_id' => 'el_id',
-				'url' => 'el_to'
+				'count' => 'COUNT(el_to)',
+				'url' => 'el_to',
+				'translated_str' => 'translated_str'
+			],
+			'join_conds' => [
+				LinkTranslations::TABLE => [ "LEFT OUTER JOIN", [ 'el_to = original_str' ] ],
 			],
 			'conds' => [
-				'page_id = el_from',
 				"$clause $like"
 			],
-			'options' => [ 'USE INDEX' => 'el_to' ]
+			'options' => [ 'GROUP BY' => 'el_to' ]
 		];
 		return $retval;
 	}
@@ -379,84 +237,78 @@ class SpecialLinkTranslations extends QueryPage {
 		];
 	}
 
-	function formatResult( $skin, $result ) {
-		$title = new TitleValue( (int)$result->namespace, $result->title );
-		$pageLink = $this->getLinkRenderer()->makeLink( $title );
+	function outputResults( $out, $skin, $dbr, $res, $num, $offset ) {
+		$html = '
+		<table class="wikitable">
+			<tr>
+				<th>Link</th>
+				<th>Repetitions</th>
+				<th>Translation</th>
+				<th>Actions</th>
+		';
 
-		$url = $result->url;
-		$urlLink = Linker::makeExternalLink( $url, $url );
-		$add_translation_url = $this->getTitle()->getFullUrl() . '?lang=' . $this->lang . '&el_id='. $result->el_id . '&search_pattern=' . $result->url;
-		return $urlLink . ' is linked from ' . $pageLink . ' (<a href="'. $add_translation_url .'">Add Translation</a> )';
+		for ( $i = 0; $i < $num && $row = $res->fetchObject(); $i++ ) {
+			$html .= $this->formatResult( $skin, $row );
+		}
+
+		$html .= '</table>';
+
+		$out->addHTML( $html );
 	}
 
-	function saveCorrection() {
+	function formatResult( $skin, $result ) {
+		$url = $result->url;
+		$urlLink = Linker::makeExternalLink( $url, $url );
+		$link_search_url = Linker::linkKnown( Title::makeTitle(NS_SPECIAL,'LinkSearch'), $result->count, array( 'target' => '_blank' ), array( 'target' => $result->url ) );
+		$add_translation_url = $this->getTitle()->getFullUrl() . '?lang=' . $this->lang . '&url='. $result->url . '&search_pattern=' . $this->search_pattern . '&limit='. $this->limit .'&offset='. $this->offset .'&translated_str=' . $result->translated_str;
+
+		$action = "Add";
+		$externalLink = "--";
+		if( !empty( $result->translated_str ) ) {
+			$externalLink = Linker::makeExternalLink( $result->translated_str, $result->translated_str );
+			$action = "Update";
+			$add_translation_url .= '&page_action=update_translation';
+		} else {
+			$add_translation_url .= '&page_action=save_translation';
+		}
+		return '
+		<tr>
+			<td>' . $urlLink . '</td>
+			<td>' . $link_search_url . '</td>
+			<td>'. $externalLink .'</td>
+			<td><a href="'. $add_translation_url . '">' . $action . '</a></td>
+		</tr>';
+	}
+
+	function saveTranslation() {
 		$dbw = wfGetDB( DB_MASTER );
 		$request = $this->getRequest();
 
 		$original_str = $request->getVal( 'original_str' );
-		$corrected_str = $request->getVal( 'corrected_str' );
+		$translated_str = $request->getVal( 'translated_str' );
 		$this->lang = $request->getVal( 'lang' );
 
 		$dbw->insert(
 			LinkTranslations::TABLE,
-			[ 'lang' => $this->lang, 'original_str' => $original_str, 'corrected_str' => $corrected_str ],
+			[ 'lang' => $this->lang, 'original_str' => $original_str, 'translated_str' => $translated_str ],
 			__METHOD__
 		);
 	}
 
-	function deleteCorrection() {
-		$dbr = wfGetDB( DB_SLAVE );
+	function updateTranslation() {
 		$dbw = wfGetDB( DB_MASTER );
 		$request = $this->getRequest();
 
-		$current_offset = $request->getVal( 'page_offset' );
-		$this->lang = $request->getVal( 'lang' );
-
-		$correction_id = $request->getVal( 'correction_id' );
-
-		$result = $dbr->delete(
-			LinkTranslations::TABLE,
-			[ 'id' => $correction_id, 'lang' => $this->lang ],
-			__METHOD__
-		);
-		if ( $result ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	function editCorrection() {
-		$dbr = wfGetDB( DB_SLAVE );
-		$dbw = wfGetDB( DB_MASTER );
-		$request = $this->getRequest();
-
-		$current_offset = $request->getVal( 'page_offset' );
-		$this->lang = $request->getVal( 'lang' );
-
-		$correction_id = $request->getVal( 'correction_id' );
-
-		$row = $dbr->selectRow( 
-			LinkTranslations::TABLE,
-			[ 'original_str', 'corrected_str' ],
-			[ 'id' => $correction_id, 'lang' => $this->lang ],
-			__METHOD__
-		);
 		$original_str = $request->getVal( 'original_str' );
-		$corrected_str = $request->getVal( 'corrected_str' );
-		if ( empty( $original_str ) || empty( $corrected_str ) ) {
-			return false;
-		}
-		if ( $original_str != $row->original_str || $corrected_str != $row->corrected_str ) {
-			$dbw->update(
-				LinkTranslations::TABLE,
-				[ 'original_str' => $original_str, 'corrected_str' => $corrected_str ],
-				array( 'id' => $correction_id ),
-				__METHOD__
-			);
-			return true;
-		}
-		return false;
+		$translated_str = $request->getVal( 'translated_str' );
+		$this->lang = $request->getVal( 'lang' );
+
+		$dbw->update(
+			LinkTranslations::TABLE,
+			[ 'lang' => $this->lang, 'translated_str' => $translated_str ],
+			[ 'original_str' => $original_str ],
+			__METHOD__
+		);
 	}
 
 }
--- includes/sql/translation_corrections.sql
+++ includes/sql/translation_corrections.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS /*_*/translation_corrections (
 	`id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`lang` varchar(50) NOT NULL,
 	`original_str` BLOB,
-	`corrected_str` BLOB
+	`translated_str` BLOB
 ) /*$wgDBTableOptions*/;
